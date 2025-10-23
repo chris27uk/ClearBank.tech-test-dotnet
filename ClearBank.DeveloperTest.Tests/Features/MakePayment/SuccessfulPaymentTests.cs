be32dc8bcb697469ca4a59059e49b2dcd0a790ea
Chris Green <chris@openemail.org>
2025-10-23 18:20:01 +0100

    Debit test.

--- ClearBank.DeveloperTest.Tests/Features/MakePayment/SuccessfulPaymentTests.cs
+++ ClearBank.DeveloperTest.Tests/Features/MakePayment/SuccessfulPaymentTests.cs
@@ -1,6 +1,7 @@
 using ClearBank.DeveloperTest.Tests.Infrastructure;
 using ClearBank.DeveloperTest.Types;
 using NUnit.Framework;
+using System.Linq;
 
 namespace ClearBank.DeveloperTest.Tests.Features.MakePayment
 {
@@ -20,6 +21,22 @@ namespace ClearBank.DeveloperTest.Tests.Features.MakePayment
             var result = sut.MakePayment(PaymentTestSubject.CreatePaymentRequest(paymentScheme: scheme));
 
             Assert.That(result.Success, Is.True);
+        }
+        
+        [TestCase(PaymentScheme.Bacs, AllowedPaymentSchemes.Bacs)]
+        [TestCase(PaymentScheme.Chaps, AllowedPaymentSchemes.Chaps)]
+        [TestCase(PaymentScheme.FasterPayments, AllowedPaymentSchemes.FasterPayments)]
+        public void Given_A_Valid_Request_When_Paying_Then_Account_Is_Debited(PaymentScheme scheme, AllowedPaymentSchemes allowedPaymentSchemes)
+        {
+            var context = PaymentTestSubject.WithExpectedPaymentResponse(accountsInPrimaryDataStore:
+            [
+                PaymentTestSubject.CreateAccount(balance: 50, allowedPaymentSchemes: allowedPaymentSchemes)
+            ]);
+            var sut = context.CreatePaymentService();
+        
+            _ = sut.MakePayment(PaymentTestSubject.CreatePaymentRequest(paymentScheme: scheme, amount: 50));
+        
+            Assert.That(context.PrimaryAccountDataStore.Updates.Single().Balance, Is.EqualTo(0));
         }
     }
 }
